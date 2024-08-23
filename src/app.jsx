97c4950ae7235693aea59b24ab0af3618edfe298
cheng <chengharvp@gmail.com>
2024-08-22 23:03:28 -0400

    9

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,31 @@
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
 import SearchParams from "./SearchParams";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt ME!</h1>
-      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
-    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-    <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt ME!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 
